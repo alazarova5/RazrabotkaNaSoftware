acd1f8c6fb3429b284f04fcbd6465b7572b7c47e
Sandy <124555898+alazarova5@users.noreply.github.com>
2023-03-27 09:28:38 +0300

    Adding BookShopSchool

---
+++ BookShopSchool/BookShopSchool/Data/Models/Author.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSchool.Data
+{
+    public class Author
+    {
+        public Author()
+        {
+            this.AuthorsBooks = new HashSet<AuthorBook>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string FName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string LName { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}
---
+++ BookShopSchool/BookShopSchool/Program.cs
@@ -0,0 +1,14 @@
+﻿using BookShopSchool.Data;
+using System;
+
+namespace BookShopSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopSchoolContext();
+            context.Database.EnsureCreated();
+        }
+    }
+}
